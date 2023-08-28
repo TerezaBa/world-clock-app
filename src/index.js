fad6b1479954740ba4261c155134f60a09657df9
TerezaBa <124687117+TerezaBa@users.noreply.github.com>
2023-08-28 12:29:46 +0200

    Added updateTime function to index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-setInterval(function () {
+function updateTime() {
+  // Prague
   let pragueElement = document.querySelector("#prague");
   let pragueDate = pragueElement.querySelector(".date");
   let pragueTime = pragueElement.querySelector(".time");
   let pragueZone = moment().tz("Europe/Prague");
-
   pragueDate.innerHTML = pragueZone.format("dddd Do MMMM");
   pragueTime.innerHTML = pragueZone.format("hh:mm:ss [<small>]A[</small>]");
 
+  // Bengaluru
   let bengaluruElement = document.querySelector("#bengaluru");
   let bengaluruDate = bengaluruElement.querySelector(".date");
   let bengaluruTime = bengaluruElement.querySelector(".time");
   let bengaluruZone = moment().tz("Asia/Kolkata");
-
   bengaluruDate.innerHTML = bengaluruZone.format("dddd Do MMMM");
   bengaluruTime.innerHTML = bengaluruZone.format(
     "hh:mm:ss [<small>]A[</small>]"
   );
-}, 1000);
+}
+
+updateTime();
+setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let timeZone = event.target.value;

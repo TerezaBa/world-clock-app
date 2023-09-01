e7bafa06c2f13a8c5220a40d0379b4126bec1bd9
TerezaBa <124687117+TerezaBa@users.noreply.github.com>
2023-09-01 16:52:33 +0200

    Added search form, timezonedb API, locationIQ API

--- src/index.js
+++ src/index.js
@@ -63,3 +63,53 @@ function updateCity(event) {
 
 let selectedCity = document.querySelector("#city");
 selectedCity.addEventListener("change", updateCity);
+
+function showTime(response) {
+  let timestamp = moment.unix(response.data.timestamp);
+  console.log(response.data);
+  clearInterval(myInterval);
+  document.querySelector(
+    "#cities"
+  ).innerHTML = `<div class="d-flex justify-content-between pt-4">
+    <div class="city">
+      <h2>${response.data.cityName}</h2>
+      <div class="date opacity-75">${timestamp.format("dddd Do MMMM")}</div>
+    </div>
+    <div class="text-end time">
+      ${timestamp.format("hh:mm")}
+      <small>${timestamp.format("A")}</small>
+    </div>
+  </div>`;
+
+  moment.unix(response.data.timestamp).format("dddd Do MMMM");
+}
+
+function getTimezone(coordinates) {
+  let apiKey = "OA2X7WH81GI9";
+  let apiUrl = `http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${coordinates.lat}&lng=${coordinates.lon}`;
+  //console.log(apiUrl);
+  axios.get(apiUrl).then(showTime);
+}
+
+function handleCoordResponse(response) {
+  // console.log(response.data[0]);
+  // let lat = response.data[0].lat;
+  // let lon = response.data[0].lon;
+  // console.log(lat, lon);
+  getTimezone(response.data[0]);
+}
+
+function getCoord(cityName) {
+  let apiKey = "pk.34a2da02bf495a4c3065a77d06788edf";
+  let apiUrl = `https://eu1.locationiq.com/v1/search?key=${apiKey}&q=${cityName}&format=json`;
+  axios.get(apiUrl).then(handleCoordResponse);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let city = document.querySelector("#search-area").value;
+  getCoord(city);
+}
+
+document.querySelector("#search-form").addEventListener("submit", handleSubmit);

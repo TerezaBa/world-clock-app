5f69910b0a5a23c5cfdb40141e3ca8771c4d3bd7
TerezaBa <124687117+TerezaBa@users.noreply.github.com>
2023-09-08 16:22:19 +0200

    Added working textbox to search for cities and their timezones
    - SheCodes weather API
    - TimezoneDB API

--- src/index.js
+++ src/index.js
@@ -64,45 +64,72 @@ function updateCity(event) {
 let selectedCity = document.querySelector("#city");
 selectedCity.addEventListener("change", updateCity);
 
-function showTime(response) {
-  let timestamp = moment.unix(response.data.timestamp);
-  console.log(response.data);
+let globalCityName = "Tokyo";
+
+function showTime(response, city) {
+  console.log(response, city);
+
+  console.log(response.timestamp);
+  let timestamp = moment.unix(response.timestamp);
   clearInterval(myInterval);
   document.querySelector(
     "#cities"
   ).innerHTML = `<div class="d-flex justify-content-between pt-4">
     <div class="city">
-      <h2>${response.data.cityName}</h2>
+      <h2>${city}</h2>
       <div class="date opacity-75">${timestamp.format("dddd Do MMMM")}</div>
     </div>
     <div class="text-end time">
       ${timestamp.format("hh:mm")}
       <small>${timestamp.format("A")}</small>
     </div>
-  </div>`;
-
-  moment.unix(response.data.timestamp).format("dddd Do MMMM");
+    </div>`;
 }
 
-function getTimezone(coordinates) {
+function getTimezone(coordinates, city) {
+  let lat = coordinates.latitude;
+  let lon = coordinates.longitude;
+  let cityName = city;
+  //globalCityName = city;
+
+  //Geonames
+  // let apiUrl = `http://api.geonames.org/timezoneJSON?lat=${response.data.coordinates.latitude}&lng=${response.data.coordinates.longitude}&username=terezaba`;
+  // console.log(apiUrl);
+  // axios.get(apiUrl).then(showTime);
+
+  // // TimezoneDB API
   let apiKey = "OA2X7WH81GI9";
-  let apiUrl = `http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${coordinates.lat}&lng=${coordinates.lon}`;
-  //console.log(apiUrl);
-  axios.get(apiUrl).then(showTime);
+  let apiUrl = `http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lon}`;
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then((response) => {
+    showTime(response.data, cityName);
+  });
 }
 
 function handleCoordResponse(response) {
+  let coordinates = response.data.coordinates;
+  let city = response.data.city;
   // console.log(response.data[0]);
   // let lat = response.data[0].lat;
   // let lon = response.data[0].lon;
   // console.log(lat, lon);
-  getTimezone(response.data[0]);
+  console.log(coordinates, city);
+  getTimezone(coordinates, city);
 }
 
 function getCoord(cityName) {
-  let apiKey = "pk.34a2da02bf495a4c3065a77d06788edf";
-  let apiUrl = `https://eu1.locationiq.com/v1/search?key=${apiKey}&q=${cityName}&format=json`;
+  // SheCodes weather API
+  let apiKey = "b36tedd42903o5c6c68a4a10b4b1953f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}`;
+
+  console.log(apiUrl);
   axios.get(apiUrl).then(handleCoordResponse);
+
+  // LocationIQ
+  // let apiKey = "pk.34a2da02bf495a4c3065a77d06788edf";
+  // let apiUrl = `https://eu1.locationiq.com/v1/search?key=${apiKey}&q=${cityName}&format=json`;
+  // axios.get(apiUrl).then(handleCoordResponse);
 }
 
 function handleSubmit(event) {

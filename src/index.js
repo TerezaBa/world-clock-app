1c48de7b6d08f1a716e94b475e54bd3ddb742109
TerezaBa <124687117+TerezaBa@users.noreply.github.com>
2023-09-08 17:34:53 +0200

    Deleted function handling select, handleCoordResponse function, logs, comments

--- src/index.js
+++ src/index.js
@@ -42,100 +42,43 @@ function updateTime() {
 updateTime();
 const myInterval = setInterval(updateTime, 1000);
 
-function updateCity(event) {
-  clearInterval(myInterval);
-  let timeZone = event.target.value;
-  if (timeZone === "current") {
-    timeZone = moment.tz.guess();
-  }
-  let cityTime = moment().tz(timeZone);
-  let citiesElement = document.querySelector("#cities");
-  let cityName = timeZone.replace("_", " ").split("/")[1];
-  if (timeZone === "America/Los_Angeles") {
-    cityName = "San Francisco";
-  }
-  citiesElement.innerHTML = `<div class="d-flex justify-content-between pt-4">
-    <div class="city">
-      <h2>${cityName}</h2>
-      <div class="date opacity-75">${cityTime.format("dddd Do MMMM")}</div>
-    </div>
-    <div class="text-end time">
-      ${cityTime.format("hh:mm")}
-      <small>${cityTime.format("A")}</small>
-    </div>
-  </div>`;
-
-  let h1Element = document.querySelector("h1");
-  h1Element.innerHTML = `<h1 class="text-center fw-bold mb-4"><a href="/">World Clock</a></h1>`;
-}
-
 function showTime(response, city) {
-  console.log(response, city);
-
-  console.log(response.timestamp);
-  let timestamp = moment.unix(response.timestamp);
-
-  console.log(moment("2023-09-08 20:00:27").format("dddd Do MMMM hh:mm:ss"));
-  console.log(timestamp);
   clearInterval(myInterval);
+
+  let currentTime = moment(response.formatted);
   document.querySelector(
     "#cities"
   ).innerHTML = `<div class="d-flex justify-content-between pt-4">
     <div class="city">
       <h2>${city}</h2>
-      <div class="date opacity-75">${timestamp.format("dddd Do MMMM")}</div>
+      <div class="date opacity-75">${currentTime.format("dddd Do MMMM")}</div>
     </div>
     <div class="text-end time">
-      ${timestamp.format("hh:mm")}
-      <small>${timestamp.format("A")}</small>
+      ${currentTime.format("hh:mm")}
+      <small>${currentTime.format("A")}</small>
     </div>
     </div>`;
+
+  let h1Element = document.querySelector("h1");
+  h1Element.innerHTML = `<h1 class="text-center fw-bold mb-4"><a href="/">World Clock</a></h1>`;
 }
 
-function getTimezone(coordinates, city) {
-  let lat = coordinates.latitude;
-  let lon = coordinates.longitude;
-  let cityName = city;
-  //globalCityName = city;
+function getTimezone(response) {
+  let lat = response.data.coordinates.latitude;
+  let lon = response.data.coordinates.longitude;
+  let cityName = response.data.city;
 
-  //Geonames
-  // let apiUrl = `http://api.geonames.org/timezoneJSON?lat=${response.data.coordinates.latitude}&lng=${response.data.coordinates.longitude}&username=terezaba`;
-  // console.log(apiUrl);
-  // axios.get(apiUrl).then(showTime);
-
-  // // TimezoneDB API
   let apiKey = "OA2X7WH81GI9";
-  let apiUrl = `http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lon}`;
-  console.log(apiUrl);
-
+  let apiUrl = `https://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lon}`;
   axios.get(apiUrl).then((response) => {
     showTime(response.data, cityName);
   });
 }
 
-function handleCoordResponse(response) {
-  let coordinates = response.data.coordinates;
-  let city = response.data.city;
-  // console.log(response.data[0]);
-  // let lat = response.data[0].lat;
-  // let lon = response.data[0].lon;
-  // console.log(lat, lon);
-  console.log(coordinates, city);
-  getTimezone(coordinates, city);
-}
-
 function getCoord(cityName) {
-  // SheCodes weather API
   let apiKey = "b36tedd42903o5c6c68a4a10b4b1953f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}`;
-
-  console.log(apiUrl);
-  axios.get(apiUrl).then(handleCoordResponse);
-
-  // LocationIQ
-  // let apiKey = "pk.34a2da02bf495a4c3065a77d06788edf";
-  // let apiUrl = `https://eu1.locationiq.com/v1/search?key=${apiKey}&q=${cityName}&format=json`;
-  // axios.get(apiUrl).then(handleCoordResponse);
+  axios.get(apiUrl).then(getTimezone);
 }
 
 function handleSubmit(event) {
